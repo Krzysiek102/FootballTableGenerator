1cbefa31353b8218e9561680a2492aae3d451dc1
Krzysiek102 <Krzysztof.Kurek102@gmail.com>
2015-11-16 08:23:35 +0100

    Simplify tests for comparator

--- FootbalTableGenerator.Core.Tests/TeamResultsSummaryComparatorTests.cs
+++ FootbalTableGenerator.Core.Tests/TeamResultsSummaryComparatorTests.cs
@@ -10,44 +10,16 @@ namespace FootbalTableGenerator.Core.Tests
     [TestFixture]
     public class TeamResultsSummaryComparatorTests
     {
-        private TeamResultsSummary team1;
-        private TeamResultsSummary team2;
-        private TeamResultsSummaryComparator comparator;
-
-        [SetUp]
-        public void Setup()
-        {
-            team1 = new TeamResultsSummary();
-            team2 = new TeamResultsSummary();
-            comparator = new TeamResultsSummaryComparator();
-        }
-
         [Test]
         public void TeamWithLargerNumberOfPointsShouldBeGreater()
         {
             //Arrane
-            team1.Points = 6;
-            team2.Points = 4;
+            TeamResultsSummary team1 = new TeamResultsSummary();
+            TeamResultsSummary team2 = new TeamResultsSummary();
+            TeamResultsSummaryComparator comparator = new TeamResultsSummaryComparator();
 
-            //Act
-            int comparisonResult = comparator.Compare(team1, team2);
-
-            //Assert
-            Assert.AreEqual(1, comparisonResult);
-        }
-
-        [Test]
-        public void TeamWithEqualPointsAndBetterGoalDifferenceShuldBeGreater()
-        {
-            //Arrane
-            team1.Points = 6;
-            team2.Points = 6;
-
-            team1.GoalsScored = 4;
-            team1.GoalsLost = 1;
-
-            team2.GoalsScored = 2;
-            team2.GoalsLost = 0;
+            team1.Points = 3;
+            team2.Points = 0;
 
             //Act
             int comparisonResult = comparator.Compare(team1, team2);
